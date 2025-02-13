db8f08f9076e24e9742f2167fdde9d3edfed90d7
Azam <azamumarov182@gmail.com>
2025-02-13 14:43:50 +0600

    some minor changes

--- src/EBook.java
+++ src/EBook.java
@@ -1,35 +1,55 @@
-public class EBook extends Book{
+public class EBook extends Book {
     private double filesizeMB;
     private String fileFormat;
-    private int pricepermb=100;
-    public EBook(String title, String author, int yearPublished, double filesizeMB, String fileFormat){
-        super(title,author,yearPublished);
-        this.filesizeMB=filesizeMB;
-        this.fileFormat=fileFormat;
+    private int pricepermb = 100;
+
+    public EBook(String title, String author, int yearPublished, double filesizeMB, String fileFormat) {
+        super(title, author, yearPublished);
+        this.filesizeMB = filesizeMB;
+        this.fileFormat = fileFormat;
     }
-    public EBook(String title, String author, int yearPublished, double filesizeMB){
-        super(title,author,yearPublished);
-        this.filesizeMB=filesizeMB;
+
+    public EBook(String title, String author, int yearPublished, double filesizeMB) {
+        super(title, author, yearPublished);
+        this.filesizeMB = filesizeMB;
     }
-    @Override public String getBookInfo(){
-        return getTitle()+","+getAuthor()+","+getYearPublished()+","+filesizeMB+","+fileFormat+",PrintedBook";
+
+    @Override
+    public String getBookInfo() {
+        return getTitle() + "," + getAuthor() + "," + getYearPublished() + "," + filesizeMB + "," + fileFormat
+                + ",PrintedBook";
     }
-    @Override public void printBookDetails(){
-        System.out.println("EBook info: \nTitle: "+getTitle()+"\nAuthor: "+getAuthor()+"\nYear Published: "+getYearPublished()+"\nFile Size: "+filesizeMB+" MB\nFile Format: "+fileFormat+"\nBook Type: EBook\n");
+
+    @Override
+    public void printBookDetails() {
+        System.out.println("EBook info: \nTitle: " + getTitle() + "\nAuthor: " + getAuthor() + "\nYear Published: "
+                + getYearPublished() + "\nFile Size: " + filesizeMB + " MB\nFile Format: " + fileFormat
+                + "\nBook Type: EBook\n");
     }
-    public void bookType(){
+
+    public void bookType() {
         System.out.println("This is an ebook");
     }
-    public double bookPrice(){
-        if("PDF".equals(fileFormat)){
-            return pricepermb*filesizeMB*1.5;
-        } else if("MOBI".equals(fileFormat)){
-            return pricepermb*filesizeMB*2;
+
+    public double bookPrice() {
+        if ("PDF".equals(fileFormat)) {
+            return pricepermb * filesizeMB * 1.5;
+        } else if ("MOBI".equals(fileFormat)) {
+            return pricepermb * filesizeMB * 2;
         } else {
-            return pricepermb*filesizeMB;
+            return pricepermb * filesizeMB;
         }
     }
-    public double getFilesizeMB(){return filesizeMB;}
-    public String getFileFormat(){return fileFormat;}
-    public int getPricepermb(){return pricepermb;}
+
+    public double getFilesizeMB() {
+        return filesizeMB;
+    }
+
+    public String getFileFormat() {
+        return fileFormat;
+    }
+
+    public int getPricepermb() {
+        return pricepermb;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-public class App{
+public class Main{
     public static void main(String[] args) {
         PrintedBook b1 = new PrintedBook("Harry Potter", "J. Roaling", 2025, 100, "Some Good Publisher");
+
         PrintedBook b2 = new PrintedBook("Harry Potter", "J. Roaling", 2024, 100);
+        
         System.out.println(b1.getBookInfo());
         System.out.println(b1.bookPrice());
         System.out.println(b2.getBookInfo());
